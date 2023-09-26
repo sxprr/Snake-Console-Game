71baf139c9a18a6df43dd942e062a019586f5daa
sxprr <magalajawad@gmail.com>
2023-09-26 21:49:51 +0100

    Drawing level walls.

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -5,9 +5,24 @@
 using namespace std;
 bool gameOver;
 
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+
 void Setup()
 {
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+
 }
 
 int main()
@@ -25,9 +40,30 @@ int main()
 
 }
 
+// Draw the map.
 void Draw()
 {
+    system("cls"); // clear the screen.
+    for (int i = 0; i < width; i++)
+        cout << "#";
+    cout << endl;
 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; i++)
+        {
+            if (j == 0)
+                cout << "#";
+            else
+                cout << " ";
+            if (j == width - 1)
+                cout << "#";
+        }
+    }
+
+    for (int i = 0; i < width; i++)
+        cout << "#";
+    cout << endl;
 
 }
 
