458ca59fd68d80281f8d2519c13de8c723608393
sxprr <magalajawad@gmail.com>
2023-09-27 23:31:48 +0100

    tail values

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -9,6 +9,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
